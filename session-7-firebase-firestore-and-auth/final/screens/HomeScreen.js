b2c37ba4855cf427e837c2d7a06bb7487fb98c8e
milesswu <mileswu13@gmail.com>
2021-02-21 10:23:41 -0800

    Make homescreen pretty

--- session-7-firebase-firestore-and-auth/final/screens/HomeScreen.js
+++ session-7-firebase-firestore-and-auth/final/screens/HomeScreen.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { 
     SafeAreaView,
     View,
+    Image,
     TouchableOpacity,
     StyleSheet,
     Text
@@ -9,19 +10,6 @@ import {
 
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { auth } from '../firebase/config'
-
-const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    title: {
-        fontSize: 28,
-        paddingBottom: 10
-    }
-  });
 
 function HomeScreen({ navigation }) {
     useEffect(() => {
@@ -50,6 +38,8 @@ function HomeScreen({ navigation }) {
         navigation.navigate('Fireside Chats');
         */
     }
+
+
     const handleLogout = async () => {
         try {
             await auth.signOut();
@@ -60,15 +50,56 @@ function HomeScreen({ navigation }) {
     }
     return (
         <SafeAreaView style={styles.container}>
-             <Text style={styles.title}>Welcome to Fireside Chats 🔥</Text>
-             <TouchableOpacity onPress={handleLogin}>
-                 <Text>Login</Text>
-             </TouchableOpacity>
-             <TouchableOpacity onPress={handleLogout}>
-                 <Text>Logout</Text>
-             </TouchableOpacity>
+            <Text style={styles.title}>Fireside Chats</Text>
+            <Image 
+                source={require('../images/fire-icon.png')}
+                style={styles.image}
+            />
+            <TouchableOpacity 
+                onPress={handleLogin}
+                style={styles.btn}
+            >
+                <Text style={styles.btnText}>Login</Text>
+            </TouchableOpacity>
+            <TouchableOpacity 
+                onPress={handleLogout}
+                style={styles.btn}
+            >
+                <Text style={styles.btnText}>Logout</Text>
+            </TouchableOpacity>
+        
         </SafeAreaView>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#ffcb2b',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    title: {
+        color: 'white',
+        fontSize: 50,
+        fontWeight: 'bold'
+    },
+    image: {
+        width: 300,
+        height: 300
+    }, 
+    btn: {
+        margin: 5,
+        paddingRight: 30,
+        paddingLeft: 30,
+        paddingTop: 15,
+        paddingBottom: 15,
+        borderRadius: 20,
+        backgroundColor: 'white'
+    },
+    btnText: {
+        color: '#ffa611'
+    }
+});
 
 export default HomeScreen;
